cc9e0fc7cb3276ee01ec518f06e73082bce9f42a
Kianapelgrim <kianapelgrim@outlook.com>
2024-03-13 20:42:35 -0600

    header

--- tareaSoft/settings.py
+++ tareaSoft/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -56,9 +57,7 @@ ROOT_URLCONF = 'tareaSoft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add the directory containing header.html
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
